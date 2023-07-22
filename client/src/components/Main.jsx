05c82639b654962fc271cc714995d09056e06cb3
femogiga <femogiga@yahoo.com>
2023-07-22 13:01:03 +0100

    working delete

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,10 +1,11 @@
-import ToDo from './ToDo'
+
 import { useEffect, useState } from "react"
 
 const Main = ({ linKVal }) => {
     const [inputValue, setInputValue] = useState("");
     const [data, setData] = useState([])
     const [style, setStyle] = useState({})
+
     const styled = {
         textDecoration: 'line-through',
         color: 'blue'
@@ -13,43 +14,38 @@ const Main = ({ linKVal }) => {
 
     const handleClick = (e) => {
         e.preventDefault();
-
+        if (inputValue == null || inputValue === "") {
+            return
+        }
         setData([...data, { id: data.length + 1, task: inputValue, completed: false }])
-
+        setInputValue("")
     };
     // console.log(data)
 
 
-
+    const handleDelete = (id) => {
+        setData(data.filter(item => item.id != id))
+    }
 
 
 
     const handleCheckboxChange = (e, itemId) => {
-        // if(e.currentTarget.checked){
-        //  setData( [...data, data.map(item=>{
-        //     if(item.id ===e.currentTarget.key){
-        //         return {...item,completed:true}
-        //     }
-        //    })])
-        // }
+
         let mapped = null
 
         if (e.currentTarget.checked) {
             setStyle(styled)
             mapped = data.map(item => {
                 if (item.id === itemId) {
-                    return { ...item, completed: e.currentTarget.checked }
+                    return { ...item, completed: !item.completed }
                 }
                 else {
+
                     return item
                 }
             })
             setData(mapped)
-
-
         }
-
-
     }
 
     console.log(data)
@@ -71,16 +67,21 @@ const Main = ({ linKVal }) => {
 
 
                     linKVal === 'Completed' ? data.filter(b => b.completed === true).map((item, index) => (<li key={index} className={`todo flex`}>
-                        <input id={item.id} type='checkbox' checked={item.completed} onChange={((e) => handleCheckboxChange(e, item.id))} /><p style={item.completed ? style : null}>{item.task}</p></li>))
+                        <input id={item.id} type='checkbox' checked={item.completed} onChange={((e) => handleCheckboxChange(e, item.id))} /><p style={item.completed ? style : null}>{item.task}</p><button onClick={() => handleDelete(item.id)}><span className="material-symbols-outlined">
+                            delete
+                        </span></button></li>))
                         : linKVal === 'Active' ? data.filter(b => b.completed === false).map((item, index) => (<li key={index} className={`todo flex`}>
-                            <input id={item.id} type='checkbox' onChange={((e) => handleCheckboxChange(e, item.id))} /><p>{item.task}</p></li>))
+                            <input id={item.id} checked={item.completed} type='checkbox' onChange={((e) => handleCheckboxChange(e, item.id))} /><p>{item.task}</p></li>))
                             : data.map((item, index) => (<li key={index} className={`todo flex`}>
                                 <input id={item.id} checked={item.completed} type='checkbox' onChange={((e) => handleCheckboxChange(e, item.id))} /><p style={item.completed ? style : null}>{item.task}</p></li>))
+
+
                 }
 
 
 
             </ul>
+            {linKVal === 'Completed' ? <button>Delete</button> : null}
         </div>
     );
 }
